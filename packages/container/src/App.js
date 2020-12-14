bc8743fcf9f1ce5bd9d902ddec36cc10fd719b64
dustinvo17 <datvo.vtd@gmail.com>
2020-12-14 16:54:58 -0600

    inital commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,10 @@
+import React , {useRef, useEffect }from 'react'
+import MarketingApp from './components/MarketingApp'
+
+export default () => {
+  return <div>
+    <h1> hi</h1>
+    <hr/>
+    <MarketingApp/>
+  </div>
+}
