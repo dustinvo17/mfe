54f99bb52c308670b39628495a21c8ef66f07ceb
dustinvo17 <datvo.vtd@gmail.com>
2020-12-15 11:55:11 -0600

    deploy marketing

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,6 @@ import React , {useRef, useEffect }from 'react'
 import MarketingApp from './components/MarketingApp'
 export default () => {
   return <div>
-    !!!!
     <hr/>
     <MarketingApp/>
   </div>
