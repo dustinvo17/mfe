ecc868106677d8e951626597547aebed33064e1d
dustinvo17 <datvo.vtd@gmail.com>
2020-12-14 18:40:45 -0600

    cleanup

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,6 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 const container = document.querySelector('#root')
-if(container) {
-    ReactDOM.render(<App /> ,container)
-}
+ReactDOM.render(<App /> ,container)
