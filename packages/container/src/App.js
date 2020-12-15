88a2baa9311cca3620879ab242cf132627fb0259
dustinvo17 <datvo.vtd@gmail.com>
2020-12-15 11:29:39 -0600

    add content to test pipeline

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,7 @@ import React , {useRef, useEffect }from 'react'
 import MarketingApp from './components/MarketingApp'
 export default () => {
   return <div>
+    !!!!
     <hr/>
     <MarketingApp/>
   </div>
