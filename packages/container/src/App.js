4832c70d5f64b1d08e2ec2e301d40aa2a280a65d
dustinvo17 <datvo.vtd@gmail.com>
2020-12-17 21:56:40 -0600

    randomize production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,22 @@
 import React , {useRef, useEffect }from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import MarketingApp from './components/MarketingApp'
+import {createGenerateClassName, StylesProvider} from '@material-ui/core/styles'
 import Header from './components/Header'
+const generateClassname = createGenerateClassName({
+  productionPrefix:'co'
+})
 export default () => {
   return <BrowserRouter>
+     <StylesProvider generateClassName={generateClassname}>
       <div>
-      <Header/>
+     
+        <Header/>
         <MarketingApp/>
-      </div>
+   
 
+      </div>
+      </StylesProvider>
   </BrowserRouter>
 
 }
